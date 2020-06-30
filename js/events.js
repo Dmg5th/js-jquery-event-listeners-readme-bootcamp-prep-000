d7ac644e314473c4bc9c029ad6536d0a392c8c51
Daniel Gould <dmgould310@gmail.com>
2020-06-30 18:19:24 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -14,12 +14,12 @@ function frameIt(){
   $('img').on('load', function(){
     $('img').addClass('tasty');
   })
-};
+}
 function pressIt(){
   $('input').on('keydown', function(key){
     if(key.which == 71{
       alert('G was pressed');
     }
-  }
   });
+}
   
